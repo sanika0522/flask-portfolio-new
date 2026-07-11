e1d6a9f9a3dff10fff2b93a67fca4a9c509cfe28
sanika kadam <kadamsanika233@gmail.com>
2026-07-11 15:58:14 +0530

    Add files via upload

---
+++ portfolio/portfolio/static/js/script.js
@@ -0,0 +1,16 @@
+// Welcome Message
+console.log("Portfolio Loaded Successfully");
+
+// Smooth Fade Animation
+//window.onload = function(){
+//    document.body.classList.add("fade");
+//};
+
+// Back to Top Button
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 300){
+        console.log("Scrolling...");
+    }
+
+});
